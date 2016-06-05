e6f4e9690eeb479643a4ed4de38d869dda652aef
Polytrope <alexandre78ferrera@gmail.com>
2016-06-05 23:14:57 +0200

    Ocaml, l'ordi a fait pshit pshit spark

--- README.md
+++ README.md
@@ -5,16 +5,16 @@ Le but du jeu est d'être le dernier survivant.
 
 ## Description 
 
-Deux joueurs possèdent des survivants sur la carte et doivent rester envie face à une invasion de zombies.
-Le comportement de chaque joueur est décris par un automate.
-Cet automate est un morceau de la map.
-Chaque joueur influ sur les automates des autres joueurs.
+Deux joueurs possèdent des survivants sur la carte et doivent rester en vie face à une invasion de zombies.
+Le comportement de chaque joueur est décrit par un automate.
+Cet automate est aussi un morceau de la map.
+Chaque joueur influe sur les automates des autres joueurs.
 
 ## Fonctionnement 
 
 * Le joueur définit un automate qui régit le comportement de ses survivants
-* Avec l'automate on crée un tableau. Par exemple si on a 5 états on peu au maximum avoir 5 actions représentés par les transition.
-* Cet automate sera aussi un morceau du tableau. Les actions représentes donc aussi un décors sur la carte avec lequel on pourra interagir suivant l'action associé dans l'automate du joueur.
+* Avec l'automate on crée un tableau. Par exemple si on a 5 états on peut au maximum avoir 5 actions représentées par les transitions.
+* Cet automate sera aussi un morceau du tableau. Les actions représentent donc aussi un décors sur la carte avec lequel on pourra interagir suivant l'action associé dans l'automate du joueur.
 
 
 ### Algorithme de base 
--- automates.ml
+++ automates.ml
@@ -1,3 +1,12 @@
+(*
+type transition = etat * condition * action * direction  * etat * priorite
+type automate = transition list
+*)
+let automate_exemple = 
+  [(1, ScanLoin(Pomme,N), Deplacer, N, 2,1) ;
+   (1,ScanProche(Pomme,N),Ramasser,N,2,1);]
+
+
 (* LES TYPES *)
 
 type direction = N (* nord *) | S | E | O
@@ -39,5 +48,16 @@ type personnage = Allie | Ennemi | Zombie
 type cible = Decor of decor | Personnage of personnage
 
 type condition =
-|ScanDistant of cible*retour (*désigne une fonction qui retourne la direction de l'élément recherché(la cible) le plus proche à une portée donnée. Si aucun élément recherché n'est présent, retourne 0*)
+|ScanLoin of cible*retour (*désigne une fonction qui retourne la direction de l'élément recherché(la cible) le plus proche à une portée donnée. Si aucun élément recherché n'est présent, retourne 0*)
 |ScanProche of cible*retour (*fonctionne de la meme maniere mais en ne regardant que les cases adjacentes (portée 1) au personnage. Retourne alors la direction d'un ennemi si il est seul et le nombre d'ennemis sinon *)
+
+type etat = int
+type priorite = int
+type transition = etat * condition * action * direction  * etat * priorite
+type automate = transition list
+
+let (scan_loin_AD: etat -> cible -> action -> etat -> priorite -> automate) = fun src cbl act tgt pri ->
+  List.map  (fun direction -> (src, ScanLoin(cbl,Direction(direction)), act, direction , tgt, pri) ) [N;S;E;O];;
+
+let (scan_loin_AD: etat -> cible -> action -> etat -> priorite -> automate) = fun src cbl act tgt pri ->
+  List.map  (fun direction -> (src, ScanLoin(cbl,Direction(direction)), act, direction , tgt, pri) ) [N;S;E;O];;
