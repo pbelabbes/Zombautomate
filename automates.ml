bdc30b4f0e6e787c5d50f36b31c9260a82d25f1d
Polytrope <alexandre78ferrera@gmail.com>
2016-06-07 01:54:22 +0200

    creation de xml

--- automates.ml
+++ automates.ml
@@ -143,11 +143,8 @@ type priorite = int
 type transition = etat * condition * action * direction  * etat * priorite
 type automate = transition list
 
+type equipe = (automate*string) list  (* liste d'automates avec un nom de personnage associé *)
 
-
-
-
-(*On verra plus tard *)
 
 let (scan_loin_AD: etat -> cible -> action -> etat -> priorite -> automate) = fun src cbl act tgt pri ->
   List.map  (fun direction -> (src, ScanLoin(cbl,Dir(direction)), act, direction , tgt, pri) ) [N;S;E;O];;
@@ -157,3 +154,102 @@ let (scan_proche_AD: etat -> cible -> action -> etat -> priorite -> automate) = 
 
 let (presence_AD: etat -> cible -> action -> etat -> priorite -> automate) = fun src cbl act tgt pri ->
   List.map (fun (direction:direction) -> (src, Present(cbl,direction), act, direction, tgt, pri) ) [N ; S ; E ; O];;
+
+
+
+
+
+(* Ecriture fichiers *)
+
+   (* Creation des strings *)
+let (etat_to_string : etat->string) = fun etat -> string_of_int etat;;
+
+let (priorite_to_string : priorite->string) = fun priorite -> string_of_int priorite;;
+
+let (direction_to_string: direction->string) = fun d -> match d with
+  |N -> "N"
+  |E -> "E"
+  |S -> "S"
+  |O -> "O";;
+
+let (retour_to_string: retour->string) = fun r -> match r with
+  |Dir(d)-> direction_to_string d
+  |Nbr(i)-> string_of_int i;;
+
+let (cible_to_string:  cible->string) = fun c -> match c with
+  |Herbe -> "Herbe"
+  |Pomme -> "Pomme"
+  |Rocher -> "Rocher"
+  |Lapin -> "Lapin"
+  |Batte_baseball -> "Batte_baseball"
+  |Katana -> "Katana"
+  |Arbre -> "Arbre"
+  |Pousse -> "Pousse"
+  |Allie -> "Allie"
+  |Ennemi -> "Ennemi"
+  |Zombie -> "Zombie";;
+ 
+let (action_to_string: action->string) = fun a -> match a with
+|Deplacer->"Deplacer"
+|Attaquer->"Attaquer"
+|Voler->"Voler"
+|Cacher->"Cacher"
+|Ramasser->"Ramasser"
+|Planter->"Planter"
+|Arroser->"Arroser"
+|Deposer->"Deposer";;
+
+let rec (condition_to_string: condition->string) = fun c -> match c with 
+  |ScanLoin(cbl,ret)->"ScanLoin("^ cible_to_string cbl ^","^ retour_to_string ret ^")"
+  |ScanProche(cbl,ret)->"ScanProche(" ^ cible_to_string cbl ^","^ retour_to_string ret ^")"
+  |Present(cbl,dir)->"Present(" ^ cible_to_string cbl ^","^ direction_to_string dir ^")"
+  |Et(c1,c2)->"Et(" ^ condition_to_string c1 ^","^ condition_to_string c2 ^")";;
+
+   (* ecriture dans un fichier *)
+
+let (print_etat_courant: out_channel->etat->unit) = fun fic courant ->
+  output_string fic ("\n\t\t\t<etat_courant>"^etat_to_string courant ^"</etat_courant>");;
+
+let (print_condition: out_channel->condition->unit) = fun fic condition ->
+  output_string fic ("\n\t\t\t<condition>"^condition_to_string condition ^"</condition>");;
+
+let (print_action: out_channel->action->unit) = fun fic action ->
+  output_string fic ("\n\t\t\t<action>"^action_to_string action ^"</action>");;
+
+let (print_direction: out_channel->direction->unit) = fun fic direction ->
+  output_string fic ("\n\t\t\t<direction>"^direction_to_string direction ^"</direction>");;
+
+let (print_priorite: out_channel->priorite->unit) = fun fic priorite ->
+  output_string fic ("\n\t\t\t<priorite>"^priorite_to_string priorite ^"</priorite>");;
+
+let (print_etat_futur: out_channel->etat->unit) = fun fic futur ->
+  output_string fic ("\n\t\t\t<etat_futur>"^etat_to_string futur ^"</etat_futur>");;
+
+
+let (print_entete: out_channel->string->string->unit) = fun fic version encodage ->
+  output_string fic ("<?xml version=\""^version^"\" encoding=\""^ encodage ^"\"?>");;
+
+let (print_transition: out_channel->transition->unit) = fun fic trans -> let (courant,condition,action,direction,priorite,futur) = trans in print_etat_courant fic courant ; print_condition fic condition ; print_action fic action ; print_direction fic direction ; print_priorite fic priorite ; print_etat_futur fic futur;;
+
+let rec (print_automate: out_channel->automate->unit) = fun fic aut-> match aut with
+  |trans::reste-> output_string fic "\n\t\t<transition>" ; print_transition fic trans ; output_string fic "\n\t\t</transition>" ; print_automate fic reste
+  |[]-> ()
+  |_->failwith "erreur dans print_automate";;
+
+
+let (print_personnage: out_channel->automate*string->unit) = fun fic automate -> let (aut,nom) = automate in
+									       output_string fic ("\n\t<automate id =\""^nom^"\">") ; print_automate fic aut ; output_string fic "\n\t</automate>" ;;
+
+
+let rec (print_equipe: out_channel->equipe->unit) = fun fic equipe -> match equipe with
+  |aut::reste -> output_string fic "\n<persos>" ; print_personnage fic aut ; print_equipe fic reste
+  |[]->output_string fic "\n</persos>"
+  |_->failwith "souci dans print_equipe"
+
+
+
+
+(*exemple *)
+let fic = open_out "personnages_générés.xml";;
+print_personnage fic (automate_perso_1,"guerier");;
+close_out fic;;
