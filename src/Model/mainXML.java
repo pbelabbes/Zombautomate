5ed6ada04c26fb955d7e6b8874a1577ef795f0cd
nicomy <nicomy_68@hotmail.fr>
2016-06-07 02:21:23 +0200

    dédbut de création de lecteur XML

---
+++ src/Model/mainXML.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package Model;
+
+/**
+ * @author Ombre Social
+ *
+ */
+public class mainXML {
+	
+	
+	static void main (){
+		XMLReader test = new XMLReader() ;
+		test.read();
+		
+	}
+}
