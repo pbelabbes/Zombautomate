1dec8f78f5adb94cd499e034ae5d857c79fb187c
alice2909 <alice.rivoal@gmail.com>
2016-06-09 18:26:47 +0200

    javadoc+fonctions

--- src/Model/Condition.java
+++ src/Model/Condition.java
@@ -2,15 +2,24 @@ package Model;
 
 public enum Condition {
 
-	PRESENCE,
-	ET,
+	PRESENCE(Presence),
+	ET(Et),
 	//OU,
-	CASEALLIEE,
-	CASEENNEMIE,
-	CASENEUTRE,
-	SCANLOIN,
-	SCANPROCHE;	
+	CASEALLIEE(CaseAlliee),
+	CASEENNEMIE(CaseEnnemie),
+	CASENEUTRE(CaseNeutre),
+	SCANLOIN(Scanloin),
+	SCANPROCHE(Scanproche);
 	
+	Condition c1;
+	Condition c2;
+	
+	private Condition(){
+		
+	}
+	private Condition(Condition2 c1){
+		this.c1=c1;
+	}
 	
 }
 
