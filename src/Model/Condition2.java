9f573a3c672fd018d63a6f10fcabe2c9acc1bb86
alice2909 <alice.rivoal@gmail.com>
2016-06-10 13:52:57 +0200

    fonctions xml

--- src/Model/Condition2.java
+++ src/Model/Condition2.java
@@ -1,36 +1,25 @@
-/**
- * 
- */
+
+
 package Model;
 
-import java.awt.Point;
+public enum Condition2 {
 
-/**
- * @author zennouche
- *
- */
-public abstract class Condition2 {
+	PRESENCE(Presence),
+	ET(Et),
+	//OU,
+	CASEALLIEE(CaseAlliee),
+	CASEENNEMIE(CaseEnnemie),
+	CASENEUTRE(CaseNeutre),
+
+	SCANLOIN(ScanLoin),
+	SCANPROCHE(ScanProche);	
 
 	
-	/**
-	 * 
-	 * @return boolean which verifies the condition
-	 */
+	private Condition c1;
 
-	public abstract boolean execute(Cell cellule);
-
-	protected Cell getTargetedCell(char direction, Cell cellule )
-	{
-		Point p = new Point(cellule.getPosition());
-		switch(direction)
-		{
-		case 'N' : p.y%=(p.y-1); break;
-		case 'E' : p.x%=(p.x+1); break;
-		case 'S' : p.y%=(p.y+1); break;
-		case 'O' : p.x%=(p.x-1);
-		default :;
-		
-		}
-		return cellule.getEntity_on().getMap().getGrid()[p.x][p.y];
+	private Condition2(Condition c1){
+		this.c1=c1;
 	}
+	
 }
+
