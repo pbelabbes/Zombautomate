9baa1b266615049bfc9014764fcbc460c54311ea
nicomy <nicomy_68@hotmail.fr>
2016-06-10 12:01:14 +0200

    focntion length and weidth automate implementé

--- src/Model/main.java
+++ src/Model/main.java
@@ -20,23 +20,35 @@ public class main {
 	
 	
 	private int height(ArrayList<transfer> automata){
-		ArrayList<int> dejavu= new ArrayList<int>();
-		transfer El;
-		for(int i=0;i<automata.size();i++){
-			El=automata.get(i);
+//		ArrayList<transfer> dejavu= new ArrayList<Integer>();
+//		transfer El;
+		int taille = width(automata) ;
+		int nbtransition[] = new int[taille] ;
+		int max = 0 ; 
+		for (int i =0 ; i< taille ; i ++){
+			nbtransition[i] = 0 ;
 			
 		}
-		
+		for(transfer el : automata){
+			nbtransition[el.getEtat_courant()]++;
+			if (nbtransition[el.getEtat_courant()]>max){
+				max = nbtransition[el.getEtat_courant()] ;
+			}
+		}
+		return max ;
 	}
 	
-	private int width(){
-		ArrayList<int> dejavu= new ArrayList<int>();
-		transfer El;
-		for(int i=0;i<automata.size();i++){
-			El=automata.get(i);
+	private int width(ArrayList<transfer> automata){
+		//ArrayList<int> dejavu= new ArrayList<int>();
+		//transfer El;
+		int etat_max = automata.get(0).getEtat_courant();
+		for(transfer el : automata){
+			if (el.getEtat_courant()> etat_max){
+				etat_max = el.getEtat_courant();
+			}
 			
 		}
-		
+		return etat_max;
 	}
 	public main() {
 		ArrayList<ArrayList<transfer>> liste=new ArrayList<ArrayList<transfer>> ();
@@ -47,7 +59,7 @@ public class main {
 		 * 
 		 */
 		//on recupere la liste de liste 
-		XMLReader fichier = new XMLReader() ;
+		XMLReader fichier = new XMLReader() ; 
 		liste=fichier.read();
 		//on creer les personnages ainsi que les automates correpsondants
 		
