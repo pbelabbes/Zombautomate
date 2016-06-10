dd4181788f48a480d55ac71496c4409815039711
Polytrope <alexandre78ferrera@gmail.com>
2016-06-10 11:02:53 +0200

    ajout d'une fonction qui simplifie la vie dans condition2.java

--- src/Model/Condition2.java
+++ src/Model/Condition2.java
@@ -3,17 +3,34 @@
  */
 package Model;
 
+import java.awt.Point;
+
 /**
  * @author zennouche
  *
  */
 public abstract class Condition2 {
 
-	protected Character personnage;
+	Character personnage;
 	
 	/**
 	 * 
 	 * @return boolean which verifies the condition
 	 */
 	public abstract boolean execute();
+	public Cell getTargetedCell(char direction)
+	{
+		Point p = new Point(personnage.getCell().getPosition());
+		switch(direction)
+		{
+		case 'N' : p.y%=(p.y-1); break;
+		case 'E' : p.x%=(p.x+1); break;
+		case 'S' : p.y%=(p.y+1); break;
+		case 'O' : p.x%=(p.x-1);
+		default :;
+		
+		}
+		return personnage.getMap().getGrid()[p.x][p.y];
+	}
+	
 }
--- src/Model/Linked_cell.java
+++ src/Model/Linked_cell.java
@@ -12,10 +12,28 @@ public class Linked_cell extends Condition2 {
 	/* (non-Javadoc)
 	 * @see Model.Condition2#execute()
 	 */
+	
+	private char direction; //direction dans laquelle se fait l'action
+	private char id_searched; //indique si on verifie case alliee, case ennemie ou case neutre
+	
+	
+	/**
+	 * 
+	 * @param personnage le possesseur l'automate qui utilise cette condition
+	 * @param direction 'N', 'E', 'S', 'O'
+	 * @param id N : neutre, A : alliee ou E : ennemie
+	 */
+	public Linked_cell(Character personnage, char direction, char id)
+	{
+		this.personnage = personnage;
+		this.direction = direction;
+		this.id_searched = id;
+	}
 	@Override
 	public boolean execute() {
-		// TODO Auto-generated method stub
-		return false;
+
+		//TODO
+		return true;
 	}
 
 }
