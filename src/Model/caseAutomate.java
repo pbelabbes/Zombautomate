772f04e674b086377027d498b135e683872ad739
pbelabbes <pbelabbes@gmail.com>
2016-06-12 20:43:38 +0200

    Affichage map aléatoire sans Automates
    [ TROP SURCHAGE ]

--- src/Model/caseAutomate.java
+++ src/Model/caseAutomate.java
@@ -1,13 +1,13 @@
 package Model;
 
-public class caseAutomate {
+public class CaseAutomate {
 	private Condition condition;
 	private Action action;
 	private int etat_futur;
 	private int priorite;
     private char direction;
 	
-	caseAutomate(int etat_futur,Action action,Condition condition,int priorite,char direction){
+	CaseAutomate(int etat_futur,Action action,Condition condition,int priorite,char direction){
 		this.etat_futur=etat_futur;
 		this.action=action;
 		this.condition=condition;
@@ -47,10 +47,6 @@ public class caseAutomate {
 	}
 	public int getPriorite(){
 		return this.priorite;
-	}
-
-	public int etat_futur() {
-		return this.etat_futur;
 	}
 	
 }
--- src/View/DisplayCellule.java
+++ src/View/DisplayCellule.java
@@ -16,13 +16,13 @@ public class DisplayCellule extends Display implements Observer {
 	Cell cellule;
 
 	public DisplayCellule(float posX, float posY, Cell c) throws SlickException{
-		super(posX, posY, setSpriteSheet(c.getDecor()), 4);
+		super(posX, posY, setSpriteSheet(c.getDecor()), 8);
 		this.cellule = c;
 
 	}
 
 	private static SpriteSheet setSpriteSheet(Decor d) throws SlickException{
-		String spritePath = "ressources/map/tuiles/terrain_atlas.png";
+		String spritePath = "ressources/map/tuiles/terrain_atlas_2.png";
 		System.out.println(spritePath);
 		return new SpriteSheet(spritePath, SIZE, SIZE);
 	}
@@ -35,25 +35,29 @@ public class DisplayCellule extends Display implements Observer {
 
 	@Override
 	public void initAnimations() {
-		 	this.animations[0] = loadAnimation(sprite, 0, 1, 25); // Grass
-//		    this.animations[1] = loadAnimation(sprite, 26, 27, 25); //Rock
-//		    this.animations[2] = loadAnimation(sprite, 12, 13, 24); //Apple ( Tomato )
-//		    this.animations[3] = loadAnimation(sprite, 14, 15, 27); //Sprout
-//		    this.animations[4] = loadAnimation(sprite, 1, 9, 0);
-//		    this.animations[5] = loadAnimation(sprite, 1, 9, 1);
-//		    this.animations[6] = loadAnimation(sprite, 1, 9, 2);
-//		    this.animations[7] = loadAnimation(sprite, 1, 9, 3);
-		
+		this.animations[0] = loadAnimation(sprite, 0, 1, 25); // Grass
+		this.animations[1] = loadAnimation(sprite, 26, 27, 25); //Rock
+		this.animations[2] = loadAnimation(sprite, 12, 13, 24); //Apple ( Tomato )
+		this.animations[3] = loadAnimation(sprite, 14, 15, 27); //Sprout
+		this.animations[4] = loadAnimation(sprite, 13, 14, 31); //Tree
+		this.animations[5] = loadAnimation(sprite, 10, 11, 5); //Katana
+		this.animations[6] = loadAnimation(sprite, 23, 24, 20); //Baseball_bat
+		this.animations[7] = loadAnimation(sprite, 15, 16, 15);//Rabbit
+
 	}
 
 	@Override
 	public Animation getCurrentAnimation() {
 		Animation selected;
-		switch(this.cellule.getDecor()){
-
-//		case ROCK : selected = this.animations[1];break;
-//		case APPLE : selected = this.animations[2];break;
-//		case SPROUT : selected = this.animations[3];break;
+		switch(this.cellule.getDecor())
+		{
+		case ROCK : selected = this.animations[1];break;
+		case APPLE : selected = this.animations[2];break;
+		case SPROUT : selected = this.animations[3];break;
+		case TREE : selected = this.animations[4];break;
+		case KATANA : selected = this.animations[5];break;
+		case BASEBALL_BAT : selected = this.animations[6];break;
+		case RABBIT : selected = this.animations[7];break;
 		default : selected = this.animations[0]; 
 		}
 		return selected;
