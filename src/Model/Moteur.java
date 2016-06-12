ea2d42d2e9a57ae532483fc6eb202c143bd4a288
Polytrope <alexandre78ferrera@gmail.com>
2016-06-12 18:37:14 +0200

    c'est bon, le placement des automates sur une carte se fait (je déteste les erreurs à cause des copier/coller git add src/)

--- src/Model/Moteur.java
+++ src/Model/Moteur.java
@@ -99,26 +99,15 @@ public class Moteur {
 	
 	/**
 	 * permet de récuperer la hauteur maximale à envisager pour placer les automates sur la carte
-	 * @param j1 Joueur 1
-	 * @param j2 Joueur 2
+	 * @param lC Liste des survivants en jeu
 	 * @return Hauteur maximale parmi tous les automates des joueurs
 	 */
-	private static int getHmax(Player j1, Player j2)
+	private static int getHmax(ArrayList<Character> lC)
 	{
 		int h_max = 0;
-
-		ArrayList<Character> lC1 = j1.getEntities();
-		ArrayList<Character> lC2 = j2.getEntities();
 		
 		
-		//2 for each
-		for(Character c : lC1) 
-		{
-		 if(c.getAutomata().getInputs() > h_max) h_max = c.getAutomata().getInputs(); 
-
-		}
-
-		for(Character c : lC2) 
+		for(Character c : lC) 
 		{
 		 if(c.getAutomata().getInputs() > h_max) h_max = c.getAutomata().getInputs(); 
 
@@ -129,25 +118,15 @@ public class Moteur {
 	
 	/**
 	 * permet de récuperer la largeur maximale à envisager pour placer les automates sur la carte
-	 * @param j1 Joueur 1
-	 * @param j2 Joueur 2
+	 * @param lC Liste des survivants en jeu
 	 * @return Largeur maximale parmi tous les automates des joueurs
 	 */
-	private static int getWmax(Player j1, Player j2)
+	private static int getWmax(ArrayList<Character> lC)
 	{
-		ArrayList<Character> lC1 = j1.getEntities();
-		ArrayList<Character> lC2 = j2.getEntities();
 
 		int w_max = 0;
 
-		//2 for each
-		for(Character c : lC1) 
-		{
-		 if(c.getAutomata().getEtats() > w_max) w_max = c.getAutomata().getEtats();
-
-		}
-
-		for(Character c : lC2) 
+		for(Character c : lC) 
 		{
 		 if(c.getAutomata().getEtats() > w_max) w_max = c.getAutomata().getEtats();
 
@@ -161,17 +140,19 @@ public class Moteur {
 	 * @param j2 Joueur2
 	 * @return Liste de points espacés des dimensions maximales d'un automate.
 	 */
-	public static ArrayList<Point> getList_coords_automatas(Player j1, Player j2)
+	public static ArrayList<Point> getList_coords_automatas(ArrayList<Character> lC)
 	{
 		
 		ArrayList<Point> lP = new ArrayList<Point>(); //résultat qui sera retourné
 		
-		int nb_charact = j1.getEntities().size() + j2.getEntities().size(); //nombre de personnages dont on devra placer l'automate sur la carte
+		int nb_charact = lC.size(); //nombre de personnages dont on devra placer l'automate sur la carte
 		int compteur = nb_charact; //Comptera le nombre de coordonnées créées pour ne pas faire une liste inutilement trop longue de coordonnées
 		
 		//dimensions maximales d'un automate
-		int h_max = getHmax(j1,j2);
-		int w_max = getWmax(j1,j2);
+		int h_max = getHmax(lC);
+		int w_max = getWmax(lC);
+		
+//		System.out.println("Hmax = "+h_max+" et Wmax = " + w_max);
 		
 		//On place à peu près autant d'automates sur la hauteur que sur la largeur de la carte (sqrt permet d'équilibrer ces 2 dimensions)
 		for(int x = 0 ; (int) x < Math.sqrt(nb_charact) +1 ; x++)
@@ -180,7 +161,9 @@ public class Moteur {
 			{
 				//On calcule les coordonnées des automates sur la carte
 				int x_reel = (int) (x*w_max+  5*Math.random() +5*x); //on prend en compte le décalage aléatoire en ajoutant en plus : 5*x
-				int y_reel = (int) (x*h_max+  5*Math.random() +5*y);
+				int y_reel = (int) (y*h_max+  5*Math.random() +5*y);
+				
+//				System.out.println("x_reel = "+x_reel+" y_reel = " +y_reel);
 				
 				if(compteur>0) lP.add(new Point(x_reel,y_reel));
 				compteur--; //On créé autant de points qu'il y a d'automates à placer. d'où le compteur
@@ -196,19 +179,17 @@ public class Moteur {
 	 * @param j2 Joueur 2
 	 * @return une map dont les coordonnées conviennent au joueurs
 	 */
-	public static Map create_map(Player j1, Player j2)
+	public static Map create_map(ArrayList<Character> lC)
 	{
-		ArrayList<Character> l1 = j1.getEntities();
-		ArrayList<Character> l2 = j2.getEntities();
-		
-		int nb_character = l1.size() + l2.size();
+	
+		int nb_character = lC.size();
 		
 		//dimensions de la map : (voir la fonction get_list_coords_automatas(..) pour comprendre les coordonnées de la map 
-		int x_map = (int) ((5+getWmax(j1,j2))*(Math.sqrt(nb_character)+2));
-		int y_map = (int) ((5+getHmax(j1,j2))*(Math.sqrt(nb_character)+2));
+		int x_map = (int) ((5+getWmax(lC))*(Math.sqrt(nb_character)+2));
+		int y_map = (int) ((5+getHmax(lC))*(Math.sqrt(nb_character)+2));
 		return new Map(x_map,y_map);
 	}
-			 
+/*			 
 	//main
 	public static void test() {
 		ArrayList<ArrayList<transfer>> liste=new ArrayList<ArrayList<transfer>> ();
@@ -219,7 +200,7 @@ public class Moteur {
 		 * 
 		 */
 		//on recupere la liste de liste 
-		XMLReader fichier = new XMLReader() ;
+/*		XMLReader fichier = new XMLReader() ;
 		
 		//TODO remplir le path
 		liste=fichier.read("/home/zennouche/Documents/semestre6/PLA/exemple.xml");
@@ -240,11 +221,11 @@ public class Moteur {
 
 
 	}
-
+*/
 	/**
 	 * @param args
 	 */
-	/*public static void main(final String[] args) {
+	public static void main(final String[] args) {
 		XMLReader fichier = new XMLReader() ;
 		
 		//geurrier
@@ -254,8 +235,8 @@ public class Moteur {
 		//faremeur 
 		//hauteur = 34 
 		//nb etat = 3
-		ArrayList<ArrayList<transfer>> liste=fichier.read("../Zombautomate/ocaml/equipe.xml");
-		Player Zombi = new Player(0, "Zombies", 10 );
+//		ArrayList<ArrayList<transfer>> liste=fichier.read("../Zombautomate/ocaml/equipe.xml");
+//		Player Zombi = new Player(0, "Zombies", 10 );
 		
 		System.out.println("\n\n\n debut");
 //		System.out.println(Integer.toString(width(liste.get(0))));
@@ -266,7 +247,7 @@ public class Moteur {
 //		System.out.println( Integer.toString(CreateEntities(Zombi, liste).size()));
 		
 		//ajout de l'automate au perso ZOmbi ) 
-		Zombi.setEntities(CreateEntities(Zombi, liste));
+//		Zombi.setEntities(CreateEntities(Zombi, liste));
 	
 		
 		
@@ -277,7 +258,7 @@ public class Moteur {
 		
 
 		Player j1 = new Player(1 ,"Joueur 1", 10);
-		Player j2 = new Player(1 ,"Joueur 2", 10);
+		Player j2 = new Player(2 ,"Joueur 2", 10);
 		
 		j1.setEntities(CreateEntities(j1,liste1));
 		j2.setEntities(CreateEntities(j2,liste2));
@@ -285,10 +266,11 @@ public class Moteur {
 		ArrayList<Character> lC = j1.getEntities();
 		lC.addAll(j2.getEntities());
 		
-		ArrayList<Point> lP = getList_coords_automatas(j1,j2);
+		
+		ArrayList<Point> lP = getList_coords_automatas(lC);
 		System.out.println(lP.toString());
 		
-		Map carte = create_map(j1,j2);
+		Map carte = create_map(lC);
 		carte.init_map();
 		carte.setAutomatas(lC, lP);
 		
@@ -296,5 +278,5 @@ public class Moteur {
 		
 //		carte.print_automatas();
 	}
-*/
+
 }
