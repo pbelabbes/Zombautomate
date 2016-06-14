6ce23de3daa2ba850b3db014c890bae5fd978e8c
douria <douria.zen@gmail.com>
2016-06-14 11:57:15 +0200

    :)

--- src/Model/Linked_cell.java
+++ src/Model/Linked_cell.java
@@ -32,6 +32,7 @@ public class Linked_cell extends Condition {
 	public boolean execute(Cell cellule) {
         Cell ce=new Cell();
         ce=getTargetedCell(direction, cellule);
+        
 		if(ce!=null && ce.getEntity_on()!=null) {
 				if(id_searched == 'N') return ce.getEntity_on().getPlayer() == null;
 		
