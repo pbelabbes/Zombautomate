712d7fba73dfc83ed2a264063a203505300a1c6e
pbelabbes <pbelabbes@gmail.com>
2016-06-15 18:53:23 +0200

    Affichage de la map avec les personnages mais mauvais positionnement et décalage

--- src/View/WindowGame.java
+++ src/View/WindowGame.java
@@ -20,137 +20,139 @@ import org.newdawn.slick.state.StateBasedGame;
 import Model.Character;
 import Model.*;
 
-public class WindowGame extends BasicGameState {
+public class WindowGame extends BasicGame {
 	//
 	public static final int ID = 2;
-	
-    private GameContainer container;
-    private ArrayList<DisplayCharacter> characters = new ArrayList<DisplayCharacter>();
-    private DisplayCellule[][] mapDisplay;
-    public  int width,height;
-    public static int TILED_SIZE = 32;
-    public int screenWidth , screenHeight;
-    public Point mapOrigin = new Point(0,0);
-    public Map map;
-    
-	public WindowGame(ArrayList<Model.Character> charactersList,Map map) throws SlickException{
-        //super("Zombautomate by PANDAS");
-        this.map = map;
 
-        for (Model.Character character : charactersList) {
-            System.out.println(character);
-            System.out.println(character.getCell());
-            new SpriteSheet("ressources/characters/sprites/male_walkcycle.png", 64, 64);
-        	if(character instanceof Survivor){
-				characters.add(new DisplaySurvivor(character));
-			}else{
-				characters.add(new DisplayZombie(character));
-			}
-		}
-    }
-	
-	@Override
+	private GameContainer container;
+	private ArrayList<DisplayCharacter> characters = new ArrayList<DisplayCharacter>();
+	private ArrayList<Model.Character> charactersList;
+	private DisplayCellule[][] mapDisplay;
+	public  int width,height;
+	public static int TILED_SIZE = 32;
+	public int screenWidth , screenHeight;
+	public Point mapOrigin = new Point(0,0);
+	public Map map;
+
+	public WindowGame(ArrayList<Model.Character> charactersList,Map map) throws SlickException{
+		super("Zombautomate by PANDAS");
+		this.map = map;
+		this.charactersList = charactersList;
+	}
+
 	public int getID() {
 		return 2;
 	}
 
 	public void setScreenDimension(int width, int height){
-    	if(width > 0 && height > 0){
-    		this.screenWidth = width;
-    		this.screenHeight = height;
-    	}
-    }
-	
-	    public void init(GameContainer container, StateBasedGame arg1) throws SlickException{
-        this.container = container;
-        //this.map = new TiledMap("ressources/map/map2.tmx");
-    
-        //Creation map bidon
-        Cell[][] grid = new Cell[map.getWidth()][map.getHeight()];
-        for (int i = 0 ; i < grid.length;i++) {
+		if(width > 0 && height > 0){
+			this.screenWidth = width;
+			this.screenHeight = height;
+		}
+	}
+
+	public void init(GameContainer container) throws SlickException{
+		this.container = container;
+		for (Model.Character character : charactersList) {
+			if(character instanceof Survivor){
+				characters.add(new DisplaySurvivor(character));
+			}else{
+				characters.add(new DisplayZombie(character));
+			}
+		}
+
+		//Creation map bidon
+		Cell[][] grid = new Cell[map.getWidth()][map.getHeight()];
+		for (int i = 0 ; i < grid.length;i++) {
 			for ( int j = 0; j< grid[i].length;j++){
 				grid[i][j] = new Cell(new Point(i,j));
 			}
 		}
-        map.setGrid(grid);
-                
-        //Creation mapDisplay
-        mapDisplay = new DisplayCellule[map.getWidth()][map.getHeight()];
-        for(int i = 0 ; i< grid.length;i++){
-        	for (int j = 0 ; j<grid.length;j++){
-        		this.mapDisplay[i][j] = new DisplayCellule(DisplayCellule.SIZE * i,DisplayCellule.SIZE * j, map.getGrid()[i][j]);
-        	}
-        }        
-	}
-    
-    
-    @Override
-    public void keyPressed(int key, char c) {
-    	DisplayCharacter dc = characters.get(0);
-    	switch (key) {
-    		//Mouvement personnage
-            case Input.KEY_UP:    dc.setDirection(0); dc.setMoving(true); break;
-            case Input.KEY_LEFT:  dc.setDirection(1); dc.setMoving(true); break;
-            case Input.KEY_DOWN:  dc.setDirection(2); dc.setMoving(true); break;
-            case Input.KEY_RIGHT: dc.setDirection(3); dc.setMoving(true); break;
-            
-            //Mouvement Camera
-            case Input.KEY_Z: this.mapOrigin.y--;break;
-            case Input.KEY_S: this.mapOrigin.y++;break;
-            case Input.KEY_Q: this.mapOrigin.x--;break;
-            case Input.KEY_D: this.mapOrigin.x++;break;
-            
-        }
-    }
-    
-    @Override
-    public void keyReleased(int key, char c) {
-    	DisplayCharacter dc = characters.get(0);
-        dc.setMoving(false);
-    }
-    
-    @Override
-    public void render(GameContainer container, StateBasedGame arg1, Graphics g) throws SlickException {
-    	
-    	for (int screenOriginX = 0,mapOriginX = this.mapOrigin.x; screenOriginX < screenWidth && (screenOriginX/TILED_SIZE)+mapOriginX < map.getWidth(); screenOriginX += TILED_SIZE) {
-    		for (int screenOriginY = 0,mapOriginY = this.mapOrigin.y; screenOriginY < screenHeight && (screenOriginY/TILED_SIZE)+mapOriginY < map.getHeight() ; screenOriginY += TILED_SIZE) {
-    			g.drawAnimation(mapDisplay[mapOriginX+(screenOriginX/TILED_SIZE)][mapOriginY+(screenOriginY/TILED_SIZE)].getCurrentAnimation(),screenOriginX , screenOriginY);
-    		}
-		}
-    	
-    	/*for (DisplayCharacter c : characters) {
-    		float charPosY = c.getY()-mapOrigin.y-32;
-    		float charPosX = c.getX()-mapOrigin.x-32;
-    		
-    		if(charPosY > 0 && charPosY < && c.getY()>mapOrigin.y && c.getY() < mapOrigin.y+screenHeight/TILED_SIZE)
-    		g.setColor(new Color(255,255,255, .5f));
-    		g.fillOval(c.getX()-16, c.getY()-8, 32, 16);
-    		g.drawAnimation(c.getCurrentAnimation(), c.getX()-mapOrigin.x-32, c.getY()-mapOrigin.y-60);
-    	}*/
-    }
+		map.setGrid(grid);
 
-    @Override
-    public void update(GameContainer container, StateBasedGame arg1, int delta) throws SlickException {
-    	DisplayCharacter dc = characters.get(0); 
-//    	System.out.println(delta);
-    	if (dc.isMoving()) {
-    	        switch (dc.getDirection()) {
-    	            case 0: dc.setY(dc.getY() - .1f * delta); break;
-    	            case 1: dc.setX(dc.getX() - .1f * delta); break;
-    	            case 2: dc.setY(dc.getY() + .1f * delta); break;
-    	            case 3: dc.setX(dc.getX() + .1f * delta); break;
-    	        }
-    	    }
-    }
-    
-    public static void main(String[] args) throws SlickException {
-    	ArrayList<Character> lC = StateGame.jeu(1) ; 
-		Map carte = Moteur.create_map(lC);
-		carte.init_map(); 
-		carte.set_charact_position(lC);
-    	WindowGame wg = new WindowGame(lC , carte);
-        AppGameContainer app= new AppGameContainer((Game) wg,1920,1080,false);
-        wg.setScreenDimension(app.getScreenWidth(), app.getScreenHeight());
-        app.start();
-    }
-}
+		//Creation mapDisplay
+		mapDisplay = new DisplayCellule[map.getWidth()][map.getHeight()];
+		for(int i = 0 ; i< grid.length;i++){
+			for (int j = 0 ; j<grid.length;j++){
+				this.mapDisplay[i][j] = new DisplayCellule(DisplayCellule.SIZE * i,DisplayCellule.SIZE * j, map.getGrid()[i][j]);
+			}
+		}        
+	}
+
+
+	@Override
+	public void keyPressed(int key, char c) {
+		DisplayCharacter dc = characters.get(0);
+		switch (key) {
+		//Mouvement personnage
+		case Input.KEY_UP:    dc.setDirection(0); dc.setMoving(true); break;
+		case Input.KEY_LEFT:  dc.setDirection(1); dc.setMoving(true); break;
+		case Input.KEY_DOWN:  dc.setDirection(2); dc.setMoving(true); break;
+		case Input.KEY_RIGHT: dc.setDirection(3); dc.setMoving(true); break;
+
+		//Mouvement Camera
+		case Input.KEY_Z: if(this.mapOrigin.y > 0) this.mapOrigin.y--;break;
+		case Input.KEY_S: this.mapOrigin.y++;break;
+		case Input.KEY_Q: if(this.mapOrigin.x > 0)this.mapOrigin.x--;break;
+		case Input.KEY_D: this.mapOrigin.x++;break;
+
+		}
+	}
+
+	@Override
+	public void keyReleased(int key, char c) {
+		DisplayCharacter dc = characters.get(0);
+		dc.setMoving(false);
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+
+		for (int screenOriginX = 0,mapOriginX = this.mapOrigin.x; screenOriginX < screenWidth && (screenOriginX/TILED_SIZE)+mapOriginX < map.getWidth(); screenOriginX += TILED_SIZE) {
+			for (int screenOriginY = 0,mapOriginY = this.mapOrigin.y; screenOriginY < screenHeight && (screenOriginY/TILED_SIZE)+mapOriginY < map.getHeight() ; screenOriginY += TILED_SIZE) {
+				g.drawAnimation(mapDisplay[mapOriginX+(screenOriginX/TILED_SIZE)][mapOriginY+(screenOriginY/TILED_SIZE)].getCurrentAnimation(),screenOriginX , screenOriginY);
+			}
+		}
+
+		for (DisplayCharacter c : characters) {
+			System.out.println("Get :"+ c.getX()+"/"+screenWidth+" "+c.getY()+"/"+screenWidth);
+			float charPosY = c.getY()*32-mapOrigin.y;
+			float charPosX = c.getX()*32-mapOrigin.x;
+			System.out.println("CharPos : "+charPosX+"/"+screenWidth+" "+charPosY+"/"+screenWidth);
+
+			if( charPosY >= 0 && charPosY <= screenWidth && charPosX >= 0 && charPosX <= screenHeight)
+			{
+				g.setColor(new Color(255,255,255, .5f));
+				g.fillOval(charPosX-16, charPosY-8, 32, 16);
+				g.drawAnimation(c.getCurrentAnimation(), charPosX-32, charPosY-60);
+			}
+		}
+//		System.exit(0);
+	}
+
+		@Override
+		public void update(GameContainer container, int delta) throws SlickException {
+			DisplayCharacter dc = characters.get(0); 
+			//    	System.out.println(delta);
+			//    	if (dc.isMoving()) {
+			//    	        switch (dc.getDirection()) {
+			//    	            case 0: dc.setY(dc.getY() - .1f * delta); break;
+			//    	            case 1: dc.setX(dc.getX() - .1f * delta); break;
+			//    	            case 2: dc.setY(dc.getY() + .1f * delta); break;
+			//    	            case 3: dc.setX(dc.getX() + .1f * delta); break;
+			//    	        }
+			//    	    }
+		}
+
+		public static void main(String[] args) throws SlickException {
+			ArrayList<Character> lC = StateGame.jeu(1) ; 
+			Map carte = Moteur.create_map(lC);
+			carte.init_map(); 
+			carte.set_charact_position(lC);
+			WindowGame wg = new WindowGame(lC , carte);
+			AppGameContainer tmp = new AppGameContainer(null);
+			AppGameContainer app= new AppGameContainer(wg,tmp.getScreenWidth(),tmp.getScreenHeight(),false);
+			wg.setScreenDimension(app.getScreenWidth(), app.getScreenHeight());
+			app.start();
+		}
+	}
