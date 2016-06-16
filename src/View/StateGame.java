00da22b66a7d681961dce8344e51f17a53225f3a
douria <douria.zen@gmail.com>
2016-06-16 14:18:24 +0200

    :)

--- src/View/StateGame.java
+++ src/View/StateGame.java
@@ -20,6 +20,12 @@ import Model.transfer;
 
 public class StateGame extends StateBasedGame {
 
+	/**
+	 * Cette fonction permet de lire le fichié du joueur ecrit en ocaml et genere le fichier
+	 * XML correspondant et renvoie le nom du fihcier XML correspondant
+	 * @param name
+	 * @return
+	 */
 	public static String demandeautomate(String name){
 		try {
 			Runtime.getRuntime().exec("gedit "+name);
@@ -62,25 +68,35 @@ public class StateGame extends StateBasedGame {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}*/
-		
 		String fich2;
-		String fich1 =demandeautomate("user1");
-		ArrayList<ArrayList<transfer>> equipe1=fichier.read(fich1);
-		
-		Player j1 = new Player(1 ,"Joueur 1", 10);
-		j1.setEntities(Moteur.CreateEntities(j1,equipe1));
-		lC.addAll(j1.getEntities());
-		
-		if(mode==2)
-		{		 fich2=demandeautomate("V2_user2");
+		String fich1 ;
+	if(mode ==1 || mode ==4){	
+		if(mode ==1||mode==5){
+			 fich1 =demandeautomate("V1_user1");
+			
+		}
+		if(mode==2||mode==4)
+		{		 fich1 =demandeautomate("V2_user1");
+				fich2=demandeautomate("V2_user2");
 				ArrayList<ArrayList<transfer>> equipe2=fichier.read(fich2);	
 				Player j2 = new Player(2 ,"Joueur 2", 10);
 				j2.setEntities(Moteur.CreateEntities(j2,equipe2));
 				lC.addAll(j2.getEntities());
 		}
+		ArrayList<ArrayList<transfer>> equipe1=fichier.read(fich1);
+		
+		Player j1 = new Player(1 ,"Joueur 1", 10);
+		j1.setEntities(Moteur.CreateEntities(j1,equipe1));
+		lC.addAll(j1.getEntities());
+		lC.addAll(getZombies().getEntities());
+	}
 		return lC;
 	}
-	
+
+	/**
+	 * Cette fonction permet d'initialiser le joueur du zombie
+	 * @return
+	 */
 	public static Player getZombies(){
 		XMLReader fichier = new XMLReader() ;
 		
