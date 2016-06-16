2a787a84c2ae6a3fc70f39509cb5bce6931a45ae
nicomy <nicomy_68@hotmail.fr>
2016-06-16 20:09:09 +0200

    fenetre du menu OMG <3

--- src/View/StateGame.java
+++ src/View/StateGame.java
@@ -108,7 +108,7 @@ public class StateGame extends StateBasedGame {
 	
 		return lC;
 
-	}
+}
 
 	
 
@@ -127,9 +127,6 @@ public class StateGame extends StateBasedGame {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}*/
-		String fich2;
-		//<<<<<<< HEAD
-		//		String fich1 =demandeautomate("user1");
 		ArrayList<ArrayList<transfer>> equipe1=fichier.read("../Zombautomate/ocaml/equipe1.xml");//fich1);
 
 		Player j1 = new Player(1 ,"Joueur 1", 10);
@@ -138,32 +135,12 @@ public class StateGame extends StateBasedGame {
 
 		if(mode==2)
 		{		 
-			//			fich2=demandeautomate("V2_user2");
 			ArrayList<ArrayList<transfer>> equipe2=fichier.read("../Zombautomate/ocaml/equipe2.xml");//fich2);	
-			//=======
-			//		String fich1 ;
-			//	if(mode ==1 || mode ==4){	
-			//		if(mode ==1||mode==5){
-			//			 fich1 =demandeautomate("V1_user1");
-			//			
-			//		}
-			//		if(mode==2||mode==4)
-			//		{		 fich1 =demandeautomate("V2_user1");
-			//				fich2=demandeautomate("V2_user2");
-			//				ArrayList<ArrayList<transfer>> equipe2=fichier.read(fich2);	
-			//>>>>>>> fdd89ca921667f8c41bf3b3fe950ee8fa31bb1bf
 			Player j2 = new Player(2 ,"Joueur 2", 10);
 			j2.setEntities(Moteur.CreateEntities(j2,equipe2));
 			lC.addAll(j2.getEntities());
 		}
-		//		ArrayList<ArrayList<transfer>> equipe1=fichier.read(fich1);
-
-		//		Player j1 = new Player(1 ,"Joueur 1", 10);
-		//		j1.setEntities(Moteur.CreateEntities(j1,equipe1));
-		//		lC.addAll(j1.getEntities());
-		//		lC.addAll(getZombies().getEntities());
-//	}
-	return lC;
+			return lC;
 }
 	
 /**
@@ -187,12 +164,12 @@ public static Player getZombies(){
  * @throws SlickException
  */
 	public void initStatesList(GameContainer arg0) throws SlickException {
-		ArrayList<Character> lC = jeu (1) ; 
-		Map carte = Moteur.initiate_map(lC, getZombies());
-		addState(new EcranDeValidation());
+		//ArrayList<Character> lC = jeu (1) ; 
+		//Map carte = Moteur.initiate_map(lC, getZombies());
 		addState(new MainScreenGameState());
 		addState(new MenuTypeJeu()) ;
 		addState(new ContinueMenutypeJeu());
+		addState(new EcranDeValidation());
 	//	addState(new WindowGame(lC,carte) ) ;
 }
 
@@ -200,14 +177,7 @@ public StateGame() {
 	super("ZOMBAUTOMATE by PANDAS");
 }
 
-/**
- * @param args
- * @throws SlickException 
- */
-//	public static void main(String[] args) throws SlickException {
-//		new AppGameContainer(new StateGame(), 800, 600, false).start();
-//
-//	}
+
 /**
  * 
  * @param args
