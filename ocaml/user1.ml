9be0834d10fdb61593dda0e9e6c8833779537c34
nicomy <nicomy_68@hotmail.fr>
2016-06-17 20:38:14 +0200

    problème pour la rectifiaction d'automate

--- ocaml/user1.ml
+++ ocaml/user1.ml
@@ -78,5 +78,5 @@ let (simplet:automate) =
   scan_loin_AD 0 Ennemi Deplacer 0 1;;
 
 
-let equipe1 = [ (guerrier,"guerrier"); (guerrier,"guerrier");(guerrier,"guerrier"); (guerrier,"guerrier")];; 
+let equipe1 = [ (guerrier,"guerrier"); (guerrier,"guerrier");(guerrier,"guerrier")];; 
 (* let equipe = [(automate_zombie, "zombie")];; *)
