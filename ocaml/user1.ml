08306fdac0b9726c069836d703daf764f026dad3
nicomy <nicomy_68@hotmail.fr>
2016-06-17 03:00:29 +0200

    pb avec la double initialisation

--- ocaml/user1.ml
+++
@@ -1,7 +0,0 @@
-open Doc ;;
-(* Cette instruction permet d'utiliser les fonctionnalitÃ©s proposÃ©es dans la premiÃ¨re version du manuel de l'utilisateur. *)
-
-(*DÃ©finissez ici vos automates ! (le langage Ã  utiliser est l'OCaml. Veuillez vous rÃ©fÃ©rer au manuel de l'utilisateur pour crÃ©er correctement les automates. *)
-
-(* utilisez la variable equipe1 pour valider votre Ã©quipe. Le type Ã  respecter est (automate*String) list *)
-let equipe1 =
