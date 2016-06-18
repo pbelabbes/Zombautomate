497d86bea15df2ea3cc6db1a73ecb5a187928773
alice2909 <alice.rivoal@gmail.com>
2016-06-18 18:30:46 +0200

    maj alice affichage complet deplacement corrects tout bon

--- src/View/WindowGame.java
+++ src/View/WindowGame.java
@@ -205,18 +205,18 @@ public class WindowGame extends BasicGameState {
 					g.fillRect(cursorX*TILED_SIZE, cursorY*TILED_SIZE, TILED_SIZE, TILED_SIZE);
 				}
 
-				if(cCell.getCell().getEntity_on() != null){
-					if(cCell.getCell().getEntity_on() instanceof Zombie){
-						g.setColor(Color.green);						
-					}else{
-						for (DisplayCharacter displayCharacter : characters) {
-							if(displayCharacter.getCharacter() == cCell.getCell().getEntity_on()) g.setColor(((DisplaySurvivor) displayCharacter).getColor());
-						}
-
-					}
-					g.fillRect(cursorX*TILED_SIZE, cursorY*TILED_SIZE, TILED_SIZE, TILED_SIZE);
-
-				}
+//				if(cCell.getCell().getEntity_on() != null){
+//					if(cCell.getCell().getEntity_on() instanceof Zombie){
+//						g.setColor(Color.green);						
+//					}else{
+//						for (DisplayCharacter displayCharacter : characters) {
+//							if(displayCharacter.getCharacter() == cCell.getCell().getEntity_on()) g.setColor(((DisplaySurvivor) displayCharacter).getColor());
+//						}
+//
+//					}
+//					g.fillRect(cursorX*TILED_SIZE, cursorY*TILED_SIZE, TILED_SIZE, TILED_SIZE);
+//
+//				}
 			}
 		}
 	}
