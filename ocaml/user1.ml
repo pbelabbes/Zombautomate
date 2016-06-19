df232f15fca381c37b225dba8725c66cc1760b16
nicomy <nicomy_68@hotmail.fr>
2016-06-19 22:47:11 +0200

    perin git add .

--- ocaml/user1.ml
+++ ocaml/user1.ml
@@ -1,4 +1,9 @@
 open Doc ;;
+ 
+let (michael_perrin:automate) = 
+scan_loin_AD 0 Allie Deplacer 0 3
+@
+presence_AD 0 Allie Echanger 0 6;;
 
 let (guerrier: automate) =
   presence_AD 0 Katana Ramasser 1 4
@@ -77,13 +82,6 @@ let (simplet:automate) =
   @
   scan_loin_AD 0 Ennemi Deplacer 0 1;;
 
-let (suicidaire:automate) = presence_AD 0 Zombie Deplacer 0 1
 
-<<<<<<< HEAD
-let equipe1 = [ (guerrier,"guerrier"); (guerrier,"guerrier");(guerrier,"guerrier"); (guerrier,"guerrier"); (farmer,"guerrier"); (farmer,"guerrier")];; 
-=======
+let equipe1 = [(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(michael_perrin,"mich-mich");(farmer,"milene");(farmer,"milene");(farmer,"milene");(farmer,"milene");(farmer,"milene")];;
 
-let equipe1 = [ (guerrier,"guerrier"); (guerrier,"guerrier");(guerrier,"guerrier"); (guerrier,"guerrier"); (farmer,"farmer"); (farmer,"farmer")];; 
->>>>>>> 5695e4990b0e7b315b655e521ded84ba2cdff417
-(* let equipe = [(automate_zombie, "zombie")];; *)
-(*let equipe1 = [(suicidaire,"p1")];;*)
