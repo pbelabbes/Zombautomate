e359fefe3dbfa761f2d6efd34cd54dcf0053e509
douria <douria.zen@gmail.com>
2016-06-19 20:39:48 +0200

    classes :automate pas fini

--- src/View/DisplaySurvivor.java
+++ src/View/DisplaySurvivor.java
@@ -13,7 +13,7 @@ public class DisplaySurvivor extends DisplayCharacter implements Observer {
 	private Color color;
 	
 	public DisplaySurvivor(model.jeu.Character c) throws SlickException{
-		super(c, 32, "ressources/characters/sprites/male_sprite.png" );
+		super(c, 36, "ressources/characters/sprites/male_sprite.png" );
 		this.setColor();		}
 
 	@Override
